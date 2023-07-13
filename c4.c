ed077327e7c71f8ed985cabcd9abfe2c6ec972f9
Ayushi <116718528+Ayushinitp@users.noreply.github.com>
2023-07-13 18:38:14 +0530

    Add files via upload

---
+++ c4.c
@@ -0,0 +1,86 @@
+//1
+
+// #include <stdio.h>
+// int main(){
+//     int a , sum=0;
+//     printf("Enter a three digit number : ");
+//     scanf("%d",&a);
+//     while(a>0){
+//         sum=sum+a%10;
+//         a=a/10;
+//     }
+//     printf("Sum is %d",sum);
+//     return 0;
+// }
+
+
+
+//2
+
+// #include <stdio.h>
+//  int main(){
+//     char a='+';
+//     printf("Ascii code is %d",a);
+//     return 0;
+//  }
+
+
+
+
+//3
+
+// #include <stdio.h>
+// int main(){
+//     int a;
+//     float b;
+//     char ch;
+//     double t;
+//     printf("Size of int is %d\nsize of float is %d\nsize of char is %d\nsize of float is %d",sizeof(a),sizeof(b),sizeof(ch),sizeof(t));
+//     return 0;
+// }
+
+
+
+
+//4
+
+// #include <stdio.h>
+//  int main(){
+//     int a;
+//     printf("Enter a number : ");
+//     scanf("%d",&a);
+//     a=a/10*10;
+//     printf("Output : %d",a);
+//     return 0;
+//  }
+
+
+
+
+
+//5
+
+// #include <stdio.h>
+// int main(){
+//     int a,d;
+//     printf("Enter a number and a digit respectively : ");
+//     scanf("%d%d",&a,&d);
+//     a=a*10+d;
+//     printf("Output is %d",a);
+//     return 0;
+// }
+
+
+
+//7
+
+// #include <stdio.h>
+//  int main(){
+//     int a;
+//     printf("Enter a number : ");
+//     scanf("%d",&a);
+//     a=a%10*100+a/10;
+//     printf("Output is %d",a);
+//     return 0;
+//  }
+
